27ba66f12085d29fb0d971f1d51750f31a2be4a6
Alex <alexbastidas.dev@gmail.com>
2022-09-20 22:31:44 -0500

    ultmimos cambios

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Equipo(models.Model):
@@ -23,6 +24,7 @@ class Tecnico(models.Model):
     telefono = models.CharField(max_length=15)
     empresa = models.CharField(max_length=50)
 
+    
     def __str__(self):
         return self.nombre
 
@@ -31,7 +33,7 @@ class Mantenimiento(models.Model):
     tipo_mantenimiento = models.CharField(max_length=50)
     fecha_mantenimiento = models.DateField(auto_now_add=True)
     observaciones = models.TextField()
-    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE )
     tecnico = models.ForeignKey(Tecnico, on_delete=models.CASCADE)
 
     def __str__(self):
